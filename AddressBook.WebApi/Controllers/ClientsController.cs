e8701aa6ac8d2aa6c67bf90b9b43be4bd9bcc0a2
Hannes <that.other.guy.i.know@gmail.com>
2022-06-13 16:50:42 +0200

    Changes - Refactor: Model suffix in client controller - Feature: update client endpoints

--- AddressBook.WebApi/Controllers/ClientsController.cs
+++ AddressBook.WebApi/Controllers/ClientsController.cs
@@ -1,6 +1,8 @@
 ﻿using AddressBook.WebApi.Attributes;
 using AddressBook.WebApi.Data;
 using AddressBook.WebApi.Data.Entities;
+using AddressBook.WebApi.Data.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -15,20 +17,22 @@ namespace AddressBook.WebApi.Controllers
     public class ClientsController : ControllerBase
     {
         private readonly IClientRepo _clientRepo;
+        private readonly IMapper _mapper;
 
-        public ClientsController(IClientRepo clientRepo)
+        public ClientsController(IClientRepo clientRepo, IMapper mapper)
         {
             this._clientRepo = clientRepo;
+            this._mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Client>>> Get()
+        public async Task<ActionResult<IEnumerable<ClientModel>>> Get()
         {
             try
             {
                 IEnumerable<Client> clients = await _clientRepo.GetAll();
 
-                return Ok(clients);
+                return Ok(this._mapper.Map<IEnumerable<ClientModel>>(clients));
             }
             catch (Exception error)
             {
@@ -37,16 +41,16 @@ namespace AddressBook.WebApi.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<IEnumerable<Client>>> Get(int id)
+        public async Task<ActionResult<ClientModel>> Get(int id)
         {
             try
             {
                 Client client = await _clientRepo.GetById(id);
 
                 if (client == null)
-                    return NotFound();
+                    return NotFound("Id doesn't exist.");
 
-                return Ok(client);
+                return Ok(this._mapper.Map<ClientModel>(client));
             }
             catch (Exception error)
             {
@@ -62,9 +66,9 @@ namespace AddressBook.WebApi.Controllers
                 Client client = await _clientRepo.GetByEmail(email);
 
                 if (client == null)
-                    return NotFound();
+                    return NotFound("Email doesn't exist.");
 
-                return Ok(client);
+                return Ok(this._mapper.Map<ClientModel>(client));
             }
             catch (Exception error)
             {
@@ -73,16 +77,68 @@ namespace AddressBook.WebApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Client>> Post(Client client)
+        public async Task<ActionResult<Client>> Post(ClientModel model)
         {
             try
             {
+                Client existingClient = await this._clientRepo.GetByEmail(model.Email);
+
+                if (existingClient != null)
+                    return BadRequest("Email already in use.");
+
+                Client client = this._mapper.Map<Client>(model);
                 Client newClient = await this._clientRepo.Create(client);
 
                 if (await this._clientRepo.SaveChanges())
-                    return Ok(newClient);
+                    return Ok(this._mapper.Map<ClientModel>(newClient));
 
-                return BadRequest();
+                return BadRequest("Could not create new client.");
+            }
+            catch (Exception error)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, error.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Client>> Put(int id, ClientModel model)
+        {
+            try
+            {
+                Client existingClient = await this._clientRepo.GetById(id);
+
+                if (existingClient == null)
+                    return BadRequest("Id doesn't exist.");
+
+                this._mapper.Map(model, existingClient);
+
+                if (await this._clientRepo.SaveChanges())
+                    return Ok(this._mapper.Map<ClientModel>(existingClient));
+
+                return BadRequest("Could not update client.");
+            }
+            catch (Exception error)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, error.Message);
+            }
+        }
+
+        [HttpPut("{email}")]
+        public async Task<ActionResult<Client>> Put(string email, ClientModel model)
+        {
+            try
+            {
+                Client existingClient = await this._clientRepo.GetByEmail(email);
+
+                if (existingClient == null)
+                    return BadRequest("Email doesn't exist.");
+
+                this._mapper.Map(model, existingClient);
+
+                if (await this._clientRepo.SaveChanges())
+                    return Ok(this._mapper.Map<ClientModel>(existingClient));
+
+                return BadRequest("Could not update client.");
             }
             catch (Exception error)
             {
